e6a78fcc7d29258e61197f960a659c5147166dc2
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-07-18 21:48:21 -1000

    able to create an object with post and add it to buzzwords!

--- server.js
+++ server.js
@@ -10,17 +10,23 @@ var buzzwords = {
 
 app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
   res.render('index');
 });
 
 app.get('/buzzwords', (req,res) => {
-  res.send(JSON.stringify(buzzwords));
+  res.send(buzzwords);
 });
 
-var server = app.listen(3000, function () {
+app.post('/buzzwords/:buzzword/:score', (req, res) => {
+  buzzwords.buzzWords.push(req.params);
+  res.send({ "success": true });
+});
+
+var server = app.listen(8080, function () {
   var host = server.address().address;
   var port = server.address().port;
 
